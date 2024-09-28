1e58e1917fee9867b4d451bb991dfdff31cc59bc
Anton <golvcenkoanton@gmail.com>
2024-09-28 16:35:07 +0200

    Add new custom errors

--- src/interfaces/CustomErrors.sol
+++ src/interfaces/CustomErrors.sol
@@ -4,4 +4,13 @@ pragma solidity 0.8.19;
 interface RaffleCustomErrors {
     /// Indicates that user provide not enough fee value to enter a raffle.
     error Raffle_NotEnoughFee();
+
+    /// Indicates that we can't select a new winner, because time interval not yet passed.
+    error Raffle_TimeIntervalNotElapsed();
+
+    /// Indicates that tthere is an error with the prize transfer for the winner.
+    error Raffle_TransferFailed();
+
+    // Indicates that currently raffle contract is paused.
+    error Raffle_RaffleIsOnPause();
 }
