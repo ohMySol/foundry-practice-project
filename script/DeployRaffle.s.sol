96953d31f1f23f2f9c6f531ee196901a889baa3c
Anton <golvcenkoanton@gmail.com>
2024-10-02 22:25:02 +0200

    Add 'sub creation', 'sub funding' and 'consumer connection' in deploy script

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -4,9 +4,11 @@ pragma solidity 0.8.19;
 import {Script} from "forge-std/Script.sol";
 import {Raffle} from "../src/Raffle.sol";
 import {HelperConfig} from "../script/HelperConfig.s.sol";
+import {CreateSubscription, FundSubscription, AddConsumer} from "../script/Interractions.s.sol";
 
 contract DeployRaffle is Script {
     function run() public {
+        deploy();
     }
 
     function deploy() public returns(Raffle, HelperConfig) {
@@ -15,8 +17,19 @@ contract DeployRaffle is Script {
         
         if (config.subscriptionId == 0) {
             // create subscription
+            CreateSubscription createSubscription = new CreateSubscription();
+            (config.subscriptionId, config.vrfCoordinator) = 
+                createSubscription.createSubscription(config.vrfCoordinator);
+            // fund subscription
+            FundSubscription fundSubscription = new FundSubscription();
+            fundSubscription.fundSubscription(
+                config.vrfCoordinator,
+                config.subscriptionId,
+                config.linkToken
+            );
         }
 
+        // deploy Raffle contract
         vm.startBroadcast();
         Raffle raffle = new Raffle(
             config.vrfCoordinator,
@@ -28,6 +41,14 @@ contract DeployRaffle is Script {
         );
         vm.stopBroadcast();
         
+        // add Raffle contract as a consumer to subcription
+        AddConsumer addConsumer = new AddConsumer();
+        addConsumer.addNewConsumer(
+            address(raffle), 
+            config.vrfCoordinator,
+            config.subscriptionId
+        );
+
         return (raffle, helperConfig);
     }
 }
